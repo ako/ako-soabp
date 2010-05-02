9d85559f73b6d89547bf1433d5308d5a1a71c777
Andrej Koelewijn <andrej@imac-van-andrej-koelewijn-3.local>
2010-05-02 21:55:08 +0200

    Moved mail configuration to properties. In glassfish configure under enterprise server, system properties.

--- MailService/src/main/java/nl/iteye/services/mailservice/MailRouteBuilder.java
+++ MailService/src/main/java/nl/iteye/services/mailservice/MailRouteBuilder.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.iteye.services.mailservice;
 
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import org.apache.camel.builder.RouteBuilder;
 import static org.apache.camel.builder.xml.XPathBuilder.xpath;
 
@@ -17,15 +18,23 @@ import static org.apache.camel.builder.xml.XPathBuilder.xpath;
  */
 public class MailRouteBuilder extends RouteBuilder {
 
+    private static final Logger log = Logger.getLogger(MailRouteBuilder.class.
+            getName());
+
     @Override
     public void configure() throws Exception {
+        String smtpHost = System.getProperty("smtp.host","localhost");
+        String smtpUsername = System.getProperty("smtp.username",null);
+        String smtpPassword = System.getProperty("smtp.password",null);
+        log.info(
+                "smtps://" + smtpHost + "?password=" + smtpPassword + "&username=" + smtpUsername);
         from("restlet://http://localhost:8786/mail/outbox?restletMethods=post").
                 to("log:nl.iteye.services.mailservice.MailRouteBuilder").
                 to("file:///tmp/mail").
-                setHeader("to",xpath("/mail/to").stringResult()).
-                setHeader("subject",xpath("/mail/subject").stringResult()).
+                setHeader("to", xpath("/mail/to").stringResult()).
+                setHeader("subject", xpath("/mail/subject").stringResult()).
                 setBody(xpath("/mail/body")).
-                to("smtps://smtp.gmail.com?password=c4m3lt3st&username=cmltst01@gmail.com");
+                to(
+                "smtps://" + smtpHost + "?password=" + smtpPassword + "&username=" + smtpUsername);
     }
-
 }

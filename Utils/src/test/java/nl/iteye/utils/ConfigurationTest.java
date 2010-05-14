195f7ce1e7e60b029e6b685cabef13720ac16568
Andrej Koelewijn <andrej@imac-van-andrej-koelewijn-3.local>
2010-05-14 11:01:45 +0200

    Moving configuration to generic utils project

---
+++ Utils/src/test/java/nl/iteye/utils/ConfigurationTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.iteye.utils;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andrej
+ */
+public class ConfigurationTest {
+
+    public ConfigurationTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testLoadConfig() throws Exception {
+    }
+
+    @Test
+    public void testGetProperty() {
+    }
+
+}
